796f8ccbb5f93e151ba922f94f321b0cf69d5349
Panagiotis Haratsaris <57539863+panagiotisharatsaris@users.noreply.github.com>
2020-07-30 15:12:10 +0300

    some changes in maps

--- javascript/map.js
+++ javascript/map.js
@@ -1,45 +1,27 @@
 function initMap() {
-  // The location of Uluru
   var uluru = {lat: 40, lng: 10};
-  // The map, centered at Uluru
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 1.5, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map,animation:google.maps.Animation.BOUNCE});
 
-  var infowindow = new google.maps.InfoWindow({
-	  content:"Hello World!"
-	});
+	var location = 'Greece';
+	var request = new XMLHttpRequest()
+	request.open('GET', 'https://nominatim.openstreetmap.org/search?format=json&q=' + location, true)
+	request.onload = function () {
+	  var data = JSON.parse(this.response)
+	  var latlng = {lat:parseFloat(data[0].lat), lng:parseFloat(data[0].lon)}
+	  var marker = new google.maps.Marker({position: latlng, map: map,animation:google.maps.Animation.BOUNCE});
+	  var infowindow = new google.maps.InfoWindow({
+		  content:location
+		});
 
-	google.maps.event.addListener(marker, 'click', function() {
-	  infowindow.open(map,marker);
-	}); 
-
-	//const geocoder = new google.maps.Geocoder();
-	//address="Greece"
-    //geocodeAddress(geocoder, map,address);
-    
+		google.maps.event.addListener(marker, 'click', function() {
+		  infowindow.open(map,marker);
+		}); 
+     }
+	request.send();
+	
 }
 
-function geocodeAddress(geocoder, resultsMap,address) {
-    geocoder.geocode(
-      {
-        address: address
-      },
-      (results, status) => {
-        if (status === "OK") {
-          resultsMap.setCenter(results[0].geometry.location);
-          new google.maps.Marker({
-            map: resultsMap,
-            position: results[0].geometry.location
-          });
-        } else {
-          alert(
-            "Geocode was not successful for the following reason: " + status
-          );
-        }
-      }
-    );
-  }
+
 
 
