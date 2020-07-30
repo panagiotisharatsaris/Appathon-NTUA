5a036c2acf6c1acd990897390366cef3120d8770
Panagiotis Haratsaris <57539863+panagiotisharatsaris@users.noreply.github.com>
2020-07-30 18:57:42 +0300

    some changes

--- javascript/map.js
+++ javascript/map.js
@@ -1,24 +1,29 @@
-function initMap() {
-  var uluru = {lat: 40, lng: 10};
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 1.5, center: uluru});
-
+function GetMap() {
+  var map = new Microsoft.Maps.Map('#myMap');
+  map.setView({
+      mapTypeId: Microsoft.Maps.MapTypeId.road,
+      center: new Microsoft.Maps.Location(0, 0),
+      zoom: 1
+  });
 	var location = 'Greece';
 	var request = new XMLHttpRequest()
 	request.open('GET', 'https://nominatim.openstreetmap.org/search?format=json&q=' + location, true)
 	request.onload = function () {
 	  var data = JSON.parse(this.response)
 	  var latlng = {lat:parseFloat(data[0].lat), lng:parseFloat(data[0].lon)}
-	  var marker = new google.maps.Marker({position: latlng, map: map,animation:google.maps.Animation.BOUNCE});
-	  var infowindow = new google.maps.InfoWindow({
-		  content:location
-		});
+	  var pin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(parseFloat(data[0].lat), parseFloat(data[0].lon)), {
+	        title: location,
+	        subTitle: 'City Center',
+	        text: '1'
+	    });
 
-		google.maps.event.addListener(marker, 'click', function() {
-		  infowindow.open(map,marker);
-		}); 
+	//Add the pushpin to the map
+	    map.entities.push(pin);
      }
 	request.send();
+	
+
+    
 	
 }
 
