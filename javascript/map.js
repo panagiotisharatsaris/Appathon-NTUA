63752f5ab361037b6dca28b8020213fe0464211a
panagiotisharatsaris <panagiotisharatsaris@hotmail.com>
2020-08-02 22:03:24 +0300

    change in maps

--- javascript/map.js
+++ javascript/map.js
@@ -9,19 +9,19 @@ function sleep(milliseconds) {
 
 
 function GetMap() {
-  var map = new Microsoft.Maps.Map('#myMap');
+  map = new Microsoft.Maps.Map('#myMap');
   map.setView({
       mapTypeId: Microsoft.Maps.MapTypeId.road,
       center: new Microsoft.Maps.Location(0, 0),
       zoom: 1
   });
-  var pins=new Microsoft.Maps.EntityCollection();
+
   var i;
-  console.log("hi");
+  var pinLayer = new Microsoft.Maps.EntityCollection();
   for (i=0;i<result.length;i=i+2){
 	var location = result[i];
         var count=result[i+1];
-        sleep(5000)
+        sleep(1000)
 	var request = new XMLHttpRequest()
         //	request.open('GET', 'http://dev.virtualearth.net/REST/v1/Locations?maxResults=1&key=AgvK9x-Z_OlQKTFUyyZXUNl7e0zIK2eYjLNF4ZUa4p4eV8rsu7h_C4Kds6jpu4fB&countryRegion='+location, true)
 	request.open('GET', 'https://nominatim.openstreetmap.org/search?format=json&q=' + location, true)
@@ -29,18 +29,10 @@ function GetMap() {
 	  var data = JSON.parse(this.response)
 
 	  var pin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(parseFloat(data[0].lat), parseFloat(data[0].lon)));
-          pin.metadata = {
-                title: location,
-	        text: count
-            };
-          pins.push(pin);
+          
+          map.entities.push(pin);
      }
 	request.send();
 }
-
-   map.entities.push(pins); 
-	
 }
-
-
 
